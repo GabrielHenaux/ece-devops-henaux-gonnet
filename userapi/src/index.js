a1b9a93be2466dfb5ca2aa1e6c6403800e5d5c2e
gabyformula1 <gabriel.henaux@edu.ece.fr>
2023-12-05 08:49:10 +0100

    Add: routes for homepage and form submission

--- userapi/src/index.js
+++ userapi/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const userRouter = require('./routes/user')
 const bodyParser = require('body-parser')
+const path = require('path');
+
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views', 'index.html'));
+});
 
 const db = require('./dbClient')
 db.on("error", (err) => {
@@ -19,10 +25,19 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use('/user', userRouter)
 
+app.post('/votre-route-cible', (req, res) => {
+  // Traitement des données du formulaire
+  console.log(req.body); // Assurez-vous d'avoir bodyParser configuré pour parser le corps de la requête
+  res.send('Formulaire reçu');
+});
+
 const server = app.listen(port, (err) => {
   if (err) throw err
   console.log("Server listening the port " + port)
 })
+
+
+
 
 
 module.exports = server
--- userapi/src/routes/user.js
+++ userapi/src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const userController = require('../controllers/user')
 
+
 const userRouter = express.Router()
 
 userRouter
